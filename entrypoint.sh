fbba79808c00406e4997778d1c4d532772b238eb
unknown <joseliojrx25@gmail.com>
2022-11-15 07:42:58 -0300

    SVG support
    - Add SVG support.
    - Optimize the quality of PNG, JPEG and TIFF formats.
    - Update docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,27 +7,39 @@ input="$1"
 output="$2"
 format="$3"
 
-outputFormats="jpeg|bmp|tiff|png|pdf"
+outputFormats="jpeg|bmp|tiff|png|pdf|svg"
 
-# message, r, g, b
+# Echo with foreground color
+# $1 message; $2 red; $3 green; $4 blue
 color() {
-  coloredMsg="\033[38;2;$2;$3;$4m$1\033[0m"
+  echo -e "\033[38;2;$2;$3;$4m$1\033[0m"
 }
 
 error() {
   color "::  Error  :: $1" 255 0 0
-  echo -e $coloredMsg
   exit 1
 }
 
-# validates the `format` input parameter
+# Change format
+# $1 path; $2 extension
+changeFmt() {
+  local r=`echo $1 | sed -E 's/(.+\/)+(.+)\..+$/\1\2/'`
+  echo "$r.$2"
+}
+
+removeFmt() {
+  local r=`echo $1 | sed -E 's/(.+\/)+(.+)\..+$/\1\2/'`
+  echo "${r}$2"
+}
+
+# Validates the $format input parameter
 isValidFormat() {
   if [[ $1 == !($outputFormats) ]]; then
     error "Unknown format: \"$1\""
   fi
 }
 
-# check if the `.eps` exists
+# Check if the `.eps` exists
 if [ ! -f $input ]; then
   error "Parameter \"input\" does not exist in \"$input\""
 fi
@@ -45,7 +57,6 @@ if [ -z $output ]; then
 
   output="${output}${endFormat}"
   color "::  Info  :: Since \"output\" parameter is not defined, the converted file will be outputed to \"$output\"" 221 158 255
-  echo -e $coloredMsg
 fi
 
 case $output in
@@ -59,19 +70,43 @@ case $output in
     format=$defaultOutputFormat;;
 esac
 
-case $format in
-  "jpeg") device=$format;;
-   "bmp") device="bmp16m";;
-  "tiff") device="tiff24nc";;
-   "png") device="png16m";;
-   "pdf") device="pdfwrite";;
-esac
+if [[ $format == +(jpeg|tiff|png|svg) ]]; then toCairo=true; fi
 
-if [[ $format != "pdf" ]]; then
+if [[ $format == +(jpeg|tiff|png|pdf|svg) ]]; then
+  device="pdfwrite"
+  fmt=$format
+  format="pdf"
+  output=$(changeFmt $output "pdf")
+else
+  device="bmp16m"
   isRaster="-dGraphicsAlphaBits=4"
 fi
 
-$(gs -q $isRaster -sDEVICE=$device -dEPSCrop -o $output $input)
+gs -q -dBATCH -dNOPAUSE $isRaster -sDEVICE=$device -dEPSCrop -sOutputFile=$output $input
+
+if [[ $toCairo ]]; then
+  input=$output
+
+  case $fmt in
+    svg)
+      output=$(changeFmt $input "svg")
+      pdftocairo -svg $input $output;;
+    +(png|tiff))
+      output=$(removeFmt $input);
+      pdftocairo -singlefile -$fmt -transp $input $output;;
+    jpeg)
+      output=$(removeFmt $input);
+      pdftocairo -singlefile -$fmt -jpegopt quality=100,optimize=y $input $output;;
+  esac
+
+  wait
+
+  if [[ $fmt == "jpeg" ]]; then mv "$output.jpg" "$output.jpeg"; fi
+  if [[ $fmt == "tiff" ]]; then mv "$output.tif" "$output.tiff"; fi
+  
+  wait
+
+  rm $input
+fi
 
 color ":: Success :: File \"$output\" created in the workflow!" 66 245 66
-echo -e $coloredMsg
